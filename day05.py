5c39b51b35c8f4a982b90e751ff753d85291e7cd
kbaikov <konstantin.baikov@gmail.com>
2019-12-05 23:51:13 +0100

    Init day05 part2

--- day05.py
+++ day05.py
@@ -29,7 +29,7 @@ def mult(tape, register):
     return tape
 
 
-def input_(tape, register, input_value=1):
+def input_(tape, register, input_value):
     instruction_pointer = register["instruction_pointer"]
     input_value_position = tape[instruction_pointer + 1]
     tape[input_value_position] = input_value
@@ -43,10 +43,60 @@ def output_(tape, register):
     print(output)
 
 
+def jump_if_true(tape, register):
+    instruction_pointer = register["instruction_pointer"]
+    input1 = tape[instruction_pointer + 1]
+    input1 = input1 if register["parameter1_mode"] else tape[input1]
+    input2 = tape[instruction_pointer + 2]
+    input2 = input2 if register["parameter2_mode"] else tape[input2]
+    if input1 != 0:
+        register["instruction_pointer"] = input2
+    return register
+
+
+def jump_if_false(tape, register):
+    instruction_pointer = register["instruction_pointer"]
+    input1 = tape[instruction_pointer + 1]
+    input1 = input1 if register["parameter1_mode"] else tape[input1]
+    input2 = tape[instruction_pointer + 2]
+    input2 = input2 if register["parameter2_mode"] else tape[input2]
+    if input1 == 0:
+        register["instruction_pointer"] = input2
+    return register
+
+
+def less_than(tape, register):
+    instruction_pointer = register["instruction_pointer"]
+    input1 = tape[instruction_pointer + 1]
+    input1 = input1 if register["parameter1_mode"] else tape[input1]
+    input2 = tape[instruction_pointer + 2]
+    input2 = input2 if register["parameter2_mode"] else tape[input2]
+    output = tape[instruction_pointer + 3]
+    if input1 < input2:
+        tape[output] = 1
+    else:
+        tape[output] = 0
+    return tape
+
+
+def equals(tape, register):
+    instruction_pointer = register["instruction_pointer"]
+    input1 = tape[instruction_pointer + 1]
+    input1 = input1 if register["parameter1_mode"] else tape[input1]
+    input2 = tape[instruction_pointer + 2]
+    input2 = input2 if register["parameter2_mode"] else tape[input2]
+    output = tape[instruction_pointer + 3]
+    if input1 == input2:
+        tape[output] = 1
+    else:
+        tape[output] = 0
+    return tape
+
+
 def parse_opcode(opcode_number, register):
     """Parse the opcode_number and update the register accordingly"""
     params, _, opcode = str(opcode_number).rpartition("0")
-    register["opcode"] = int(opcode)
+    register["opcode"] = int(str(opcode_number)[-2:])
     register["parameter1_mode"] = register["parameter2_mode"] = register[
         "parameter3_mode"
     ] = 0
@@ -63,7 +113,7 @@ def parse_opcode(opcode_number, register):
     return register
 
 
-def process_tape(tape):
+def process_tape(tape, input_value):
     register = dict(
         instruction_pointer=0,
         opcode=0,
@@ -83,11 +133,21 @@ def process_tape(tape):
             tape = mult(tape, register)
             register["instruction_pointer"] += 4
         elif register["opcode"] == 3:
-            tape = input_(tape, register)
+            tape = input_(tape, register, input_value)
             register["instruction_pointer"] += 2
         elif register["opcode"] == 4:
             output_(tape, register)
             register["instruction_pointer"] += 2
+        elif register["opcode"] == 5:
+            register = jump_if_true(tape, register)
+        elif register["opcode"] == 6:
+            register = jump_if_false(tape, register)
+        elif register["opcode"] == 7:
+            tape = less_than(tape, register)
+            register["instruction_pointer"] += 4
+        elif register["opcode"] == 8:
+            tape = equals(tape, register)
+            register["instruction_pointer"] += 4
 
 
 if __name__ == "__main__":
@@ -95,6 +155,6 @@ if __name__ == "__main__":
     with open("day05_input.txt") as f:
         original_tape = [int(x) for x in f.readline().split(",")]
     tape = original_tape.copy()
-    result = process_tape(tape)
+    process_tape(tape, 1)  # 9961446
+    process_tape(tape, 5)  #
 
-    # print(result[0])  #
