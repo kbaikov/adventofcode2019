80ce5569d5432c12068ae1afaf8d6fadc887c20d
Konstantin Baikov <konstantin.baikov.ext@siemens-healthineers.com>
2019-12-11 16:53:12 +0100

    Progress day 08 part 2

--- day08.py
+++ day08.py
@@ -6,6 +6,7 @@
 
 # log = logging.getLogger(__name__)
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def part1(array):
@@ -17,9 +18,21 @@ def part1(array):
     return number_of_1_digits * number_of_2_digits
 
 
+def part2(array):
+    # from https://stackoverflow.com/questions/16492830/colorplot-of-2d-array-matplotlib
+    array = array.reshape(-1, 6, 25)
+    # fig = plt.figure(figsize=(6, 3.2))
+    # ax = fig.add_subplot(111)
+    # ax.set_title("colorMap")
+    plt.imshow(array[0], cmap=plt.cm.bone)
+    # ax.set_aspect("equal")
+    plt.show()
+
+
 if __name__ == "__main__":
 
     with open("day08_input.txt") as f:
         array = np.fromiter(f.readline().rstrip(), dtype=np.int)
 
     print(part1(array))  # 1206
+    part2(array)
