eae9e7f6232f89ec30846793a6686c5c47ee9091
Konstantin Baikov <konstantin.baikov.ext@siemens-healthineers.com>
2019-12-12 14:25:13 +0100

    Solve day 08 part 2

--- day08.py
+++ day08.py
@@ -8,6 +8,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import mpl_toolkits.mplot3d
+from PIL import Image
+
+
+def select_visible(a):
+    for element in a:
+        if element == 0:
+            return 0
+        elif element == 1:
+            return 1
+        elif element == 2:
+            continue
+        else:
+            return 2
+
 
 def part1(array):
     array = array.reshape(-1, 6, 25)
@@ -19,14 +34,33 @@ def part1(array):
 
 
 def part2(array):
+    # 0 is black, 1 is white, and 2 is transparent
     # from https://stackoverflow.com/questions/16492830/colorplot-of-2d-array-matplotlib
     array = array.reshape(-1, 6, 25)
+    # array[array == 0] = 255
+    # array[array == 1] = 0
+    # array[array == 2] = 155
+    z = np.apply_along_axis(select_visible, axis=0, arr=array)
+    plt.imshow(z, alpha=0.5)
+    # img = Image.fromarray(array[0])
+    # img.save("testgrey.png")
+    # img.show()
+    plt.show()
+
     # fig = plt.figure(figsize=(6, 3.2))
     # ax = fig.add_subplot(111)
     # ax.set_title("colorMap")
-    plt.imshow(array[0], cmap=plt.cm.bone)
     # ax.set_aspect("equal")
-    plt.show()
+    # fig = plt.figure()
+    # ax = fig.gca(projection="3d")
+    # voxels = array[:2]
+    # colors = np.array([[[(1.0, 2.0, 3.0, 4.0)], [(1.0, 0.0, 0.0, 0.5)], [(1.0, 0.0, 0.0, 0.5)]]])
+    # ax.voxels(voxels)
+    # fig = plt.figure(figsize=(4, 4))
+    # ax = fig.add_subplot(111, title="Test scatter")
+    # ax.scatter(array[0][0], array[0][1], s=100, color="blue", alpha=0.5)
+    # fig.savefig("test_scatter.png")
+    # plt.show()
 
 
 if __name__ == "__main__":
@@ -35,4 +69,4 @@ if __name__ == "__main__":
         array = np.fromiter(f.readline().rstrip(), dtype=np.int)
 
     print(part1(array))  # 1206
-    part2(array)
+    part2(array)  # EJRGP
